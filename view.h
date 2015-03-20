1959467311ccd43b89404bbc00885516b602e4aa
Alex Robbins <robbinsa@wit.edu>
2015-03-20 10:19:43 -0400

    view header file

--- view.h
+++ view.h
@@ -18,11 +18,18 @@ public:
     ~View();
     // Print out the visible stuff in the grid
     void show(Model * model);
+
 private:
     SDL_Window* window;
     SDL_Surface* screen;
     bool fail;
     SDL_Surface* load(char * path);
+	
+	SDL_Surface* road;
+	SDL_Surface* car;
+	SDL_Rect source_road_1, source_road_2;
+	SDL_Rect destination_road_1, destination_road_2, destination_car;
+	
 //    SDL_Surface* text;
 //    Mix_Music * music;
 //    Mix_Chunk * food;
